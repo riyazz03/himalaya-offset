6a197561001a1810394031093dbe664761e062d9
Riyaz1203 <mohammedriyaz12032003@gmail.com>
2025-10-26 02:24:28 +0530

    Profile Page Update

--- component/ClientLayout.tsx
+++ component/ClientLayout.tsx
@@ -4,8 +4,6 @@ import { ReactNode } from "react";
 import { SessionProvider } from 'next-auth/react';
 import AnnouncementBar from "@/component/AnnouncementBar";
 import Header from "@/component/Header";
-import MegaMenu from "@/component/MegaMenu";
-// import Footer from "@/component/Footer"; // Uncomment this if you have a Footer component
 
 interface ClientLayoutProps {
   children: ReactNode;
@@ -17,7 +15,6 @@ export default function ClientLayout({ children }: ClientLayoutProps) {
       <div className="app-layout">
         <AnnouncementBar />
         <Header />
-        <MegaMenu />
         <main className="main-content">
           {children}
         </main>
